58742537079e9b7b1a0b5b3308c61474210d97e8
Anthony KALBE <104859937+Beyxyy@users.noreply.github.com>
2024-02-13 17:32:04 +0100

    suite des fragment + page

--- app/src/main/java/fr/anthonykalbe/daily_tracking/MainActivity.java
+++ app/src/main/java/fr/anthonykalbe/daily_tracking/MainActivity.java
@@ -1,35 +1,19 @@
 package fr.anthonykalbe.daily_tracking;
 
-import static android.preference.PreferenceManager.getDefaultSharedPreferences;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.os.Bundle;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
-    private Button buttonClick;
 
-    private final int nav_message = R.id.nav_message;
-    private final int nav_home = R.id.nav_home;
-    private final int nav_profile = R.id.nav_profile;
-    private int count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LoginActivity login = new LoginActivity();
         if(!LoginActivity.CheckisLoggedIn(getApplicationContext())){
             Intent loginActivity = new Intent(getApplicationContext(), LoginActivity.class);
             startActivity(loginActivity);
